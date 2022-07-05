1fe643e3d891d3f6074a13f627f7ce32e48e368d
shankarmishra124 <shankarmishra124@gmail.com>
2022-07-05 17:41:02 +0530

    Added my project

---
+++ message.js
@@ -0,0 +1,25 @@
+
+
+var a = 6;
+var b = 7;
+function msg1(){
+    var z = a+b;
+    alert(z);
+
+}
+function msg2(){
+    var z = a-b;
+ 
+    alert(z);
+
+}
+
+function msg3(){
+    window.z = a-b;
+}
+function msg4(){
+    alert(z);
+}
+// document.write("This is Shankar Mishra")
+
+
